40cb7ac09a8dfe4842f50cb255307271030db2d4
Daniela Repkova <@student.inholland.nl>
2021-10-15 11:08:33 +0200

    currency conversion

--- src/main/java/nl/inholland/javafx/term1/HelloApplication.java
+++ src/main/java/nl/inholland/javafx/term1/HelloApplication.java
@@ -1,7 +1,11 @@
 package nl.inholland.javafx.term1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,16 +14,22 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage window) throws IOException {
-        window.setHeight(600);
-        window.setWidth(800);
+        window.setHeight(300);
+        window.setWidth(300);
+        DecimalFormat df2 = new DecimalFormat(".##");
 
         // Login
         window.setTitle("Login screen");
         GridPane gridPane = new GridPane();
+        gridPane.setVgap(10); // Vertical spacing between grid items
+        gridPane.setHgap(8); // Horizontal spacing between grid items
+        gridPane.setAlignment(Pos.CENTER);
+
         Label euroLabel = new Label("Amount €:");
         TextField euroValue = new TextField();
         Label dollarLabel = new Label("Dollar $:");
@@ -34,6 +44,14 @@ public class HelloApplication extends Application {
 
         gridPane.add(dollarLabel, 0, 2);
         gridPane.add(dollarValue, 1, 2);
+
+        convertBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                double result = Double.parseDouble(euroValue.getText()) * 1.18;
+                dollarValue.setText(String.valueOf(df2.format(result)));
+            }
+        });
 
         Scene scene = new Scene(gridPane);
         window.setTitle("Currency converter");
